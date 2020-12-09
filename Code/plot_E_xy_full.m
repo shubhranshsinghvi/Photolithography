0cd2d61adef46e7d25986e6690a36aa3fee2db5a
shubhranshsinghvi <shubhransh.singhvi@students.iiit.ac.in>
2020-12-10 01:08:21 +0530

    .

--- Code/plot_E_xy_full.m
+++ Code/plot_E_xy_full.m
@@ -1,7 +1,8 @@
 function plot_E_xy_full(ledcord,nleds,Ir_0,z,d)
     
 l=sqrt(nleds);
-[x,y]=meshgrid(-0.1:0.0005:0.1,-0.1:0.0005:0.1);
+plt_lim = (l-1)*d;
+[x,y]=meshgrid(-plt_lim:0.001:plt_lim,-plt_lim:0.001:plt_lim);
 
 E=zeros(size(x));
 for i = 1:nleds
@@ -16,6 +17,6 @@ colorbar
 xlabel('x-coordinate(m)');
 ylabel('y-coordinate(m)');
 zlabel('Irradinace(W/m^2)');
-zlim([0,E_max]);
+zlim([0,1.1*E_max]);
 s.EdgeColor = 'none';
 end
